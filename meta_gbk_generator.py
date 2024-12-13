aaf15efb94cead4f23feee18143b17996549ee9f
Ricardo Palma Véjares <65282476+rpalmavejares@users.noreply.github.com>
2024-12-13 00:17:49 -0300

    Update meta_gbk_generator.py

--- meta_gbk_generator.py
+++ meta_gbk_generator.py
@@ -8,7 +8,7 @@ from argparse import ArgumentParser
 import time
 import datetime 
 
-date = datetime.datetime.today().strftime('%m/%d/%Y')
+date = datetime.datetime.today().strftime('%d/%m/%Y')
 
 date_dict ={"01":"JAN","02":"FEB","03":"MAR","04":"APR","05":"MAY","06":"JUN","07":"JUL","08":"AUG","09":"SEP","10":"OCT","11":"NOV","12":"DEC"}
 
