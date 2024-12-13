eb7166a54dbbba8f81f5eaf67501024dae4a63c7
Ricardo Palma Véjares <65282476+rpalmavejares@users.noreply.github.com>
2024-12-12 22:57:44 -0300

    Update bash_example_Metagenome.sh

--- bash_example_Metagenome.sh
+++ bash_example_Metagenome.sh
@@ -1,1 +1,2 @@
+tar -xvzf example_Metagenome.tar.gz
 python meta_gbk_generator.py --assembly example_dataset/Metagenome_assembly.fasta --faa example_dataset/Metagenome_genes.faa --annot example_dataset/Metagenome_annotation.tsv --source Bacteria --organism Bacteria --taxonomy Bacteria --isolate Meta-01 --isolation_source "Marine Megagenome" --taxon_id 2 --country Chile --gbk Meta-01_genbank --type m2m
